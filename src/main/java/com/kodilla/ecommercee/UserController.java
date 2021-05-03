d6e46f3cd2cb4de43cebf807f7b4040073424aa2
ZadrogaB <zadroga.b@wp.pl>
2021-05-03 18:03:56 +0200

    Create endpoints to UserController. Add lombok and mysql connector to build gradle file

--- src/main/java/com/kodilla/ecommercee/UserController.java
+++ src/main/java/com/kodilla/ecommercee/UserController.java
@@ -1,4 +1,29 @@
 package com.kodilla.ecommercee;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/group_project/user")
+@RequiredArgsConstructor
 public class UserController {
+
+    //PLACE FOR PARAMETERS
+
+    @PostMapping(value = "createUser"/*, consumes = MediaType.APPLICATION_JSON_VALUE*/)
+    public void createUser(/*@RequestBody userDto userDto*/) {
+        
+    }
+
+    @PutMapping(value = "blockUser")
+    public void blockUser(@RequestParam int userId) {
+
+    }
+
+    @PutMapping(value = "generateKey")
+    public int generateKey(@RequestParam int userId) {
+        int key = 123456789;
+        return key;
+    }
 }
