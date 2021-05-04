eeb9a857c1c193d0fe0901c9fbc091909c307a30
Kamil Witczak <kamilgimp99@gmail.com>
2021-05-04 13:01:50 +0200

    CartController corrected and added CartDto

--- src/main/java/com/kodilla/ecommercee/CartController.java
+++ src/main/java/com/kodilla/ecommercee/CartController.java
@@ -1,4 +1,34 @@
 package com.kodilla.ecommercee;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/cart")
 public class CartController {
+
+    @PostMapping(value = "createCart")
+    public void createCart(@RequestBody CartDto cartDto){
+
+    }
+
+    @GetMapping(value = "getFromCart")
+    public List<CartDto> getFromCart(){
+        List<CartDto> cart = new ArrayList<>();
+        return cart;
+    }
+
+    @PutMapping(value = "addElementToCart")
+    public void addElementToCart(@RequestBody CartDto cartDto){
+    }
+
+    @DeleteMapping(value = "deleteElementFromCart")
+    public void deleteElementFromCart(@RequestParam Long id){
+    }
+
+    @PostMapping(value = "createOrder")
+    public void createOrder(@RequestBody CartDto cartDto){
+    }
 }
