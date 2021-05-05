bb53e1ac382bb2b34bd4aea117609650b1b48442
Sandra Owczarek <sandraowczarek5@gmail.com>
2021-05-05 17:42:23 +0200

    added @requestParam/Body

--- src/main/java/com/kodilla/ecommercee/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/ProductController.java
@@ -1,9 +1,7 @@
 package com.kodilla.ecommercee;
 
 import com.kodilla.ecommercee.Dto.ProductDto;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,19 +14,19 @@ public class ProductController {
         return new ArrayList<>();
     }
     @RequestMapping(method = RequestMethod.GET, value = "getProduct")
-    public ProductDto getProduct(Long productId) {
+    public ProductDto getProduct(@RequestParam Long productId) {
         return new ProductDto(1L, "name", "description", 1);
     }
     @RequestMapping(method = RequestMethod.DELETE, value = "deleteProduct")
-    public void deleteProduct(Long productId) {
+    public void deleteProduct(@RequestParam Long productId) {
 
     }
     @RequestMapping(method = RequestMethod.PUT, value = "updateProduct")
-    public ProductDto updateProduct(ProductDto productDto) {
+    public ProductDto updateProduct(@RequestBody ProductDto productDto) {
         return new ProductDto(1L, "name", "description", 1);
     }
     @RequestMapping(method = RequestMethod.POST, value = "createProduct")
-    public void createProduct(ProductDto productDto) {
+    public void createProduct(@RequestBody ProductDto productDto){
 
     }
 }
