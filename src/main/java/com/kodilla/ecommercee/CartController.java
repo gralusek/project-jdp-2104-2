d508723686df61522bf9cef7f0bf1abc6083877e
Dariusz Grala <gralusek@interia.pl>
2021-05-06 19:23:32 +0200

    JDP210402-4

--- src/main/java/com/kodilla/ecommercee/CartController.java
+++ src/main/java/com/kodilla/ecommercee/CartController.java
@@ -38,7 +38,6 @@ public class CartController {
     }
 
     @PostMapping(value = "createOrder")
-    public OrderDto createOrder(@RequestParam Long cartId){
-        return new OrderDto(cartId);
+    public void createOrder(@RequestBody OrderDto orderDto){
     }
 }
