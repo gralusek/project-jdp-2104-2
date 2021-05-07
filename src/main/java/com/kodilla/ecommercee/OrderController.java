ea24ed34b29f0d277d6e1403060e6bb53ec78c56
Dariusz Grala <gralusek@interia.pl>
2021-05-07 21:14:04 +0200

    JDP2104-4

--- src/main/java/com/kodilla/ecommercee/OrderController.java
+++ src/main/java/com/kodilla/ecommercee/OrderController.java
@@ -22,7 +22,7 @@ public class OrderController {
     }
 
     @GetMapping(value = "getOrder")
-    public OrderDto getOrder(@RequestParam OrderDto Orderid) {
+    public OrderDto getOrder(@RequestParam OrderDto id) {
         OrderDto exampleOrder = new OrderDto(1L,"example Order");
         return exampleOrder;
     }
@@ -34,7 +34,7 @@ public class OrderController {
     }
 
     @DeleteMapping(value = "deleteOrder")
-    public OrderDto deleteOrder(@RequestParam OrderDto Orderid) {
+    public OrderDto deleteOrder(@RequestParam OrderDto id) {
         return null;
     }
 }
