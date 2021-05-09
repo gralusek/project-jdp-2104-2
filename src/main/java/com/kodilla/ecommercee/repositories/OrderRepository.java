130b7e05f2fe6e220518d9cf32a085e82b7ff13f
ZadrogaB <zadroga.b@wp.pl>
2021-05-09 19:20:54 +0200

    conflict fixed

--- src/main/java/com/kodilla/ecommercee/repositories/OrderRepository.java
+++ src/main/java/com/kodilla/ecommercee/repositories/OrderRepository.java
@@ -1,7 +1,10 @@
 package com.kodilla.ecommercee.repositories;
 
 import com.kodilla.ecommercee.domain.Order;
+import com.kodilla.ecommercee.domain.User;
 import org.springframework.data.repository.CrudRepository;
+
+import java.util.*;
 
 public interface OrderRepository extends CrudRepository<Order, Long> {
 
