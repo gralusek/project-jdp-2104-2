2b727296f0f9a28dba2775194798947b8e972078
Sandra Owczarek <sandraowczarek5@gmail.com>
2021-05-11 15:15:56 +0200

    update on Product Controller & ProductDto

--- src/main/java/com/kodilla/ecommercee/ProductController.java
+++ src/main/java/com/kodilla/ecommercee/ProductController.java
@@ -1,4 +1,32 @@
 package com.kodilla.ecommercee;
 
+import com.kodilla.ecommercee.Dto.ProductDto;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/v1/product")
 public class ProductController {
+    @GetMapping
+    public List<ProductDto> getProducts() {
+        return new ArrayList<>();
+    }
+    @GetMapping("/{productId}/")
+    public ProductDto getProduct(@PathVariable Long productId) {
+        return new ProductDto(1L, "name", "description", 1.0);
+    }
+    @DeleteMapping("/{productId}/")
+    public void deleteProduct(@PathVariable Long productId) {
+
+    }
+    @PutMapping
+    public ProductDto updateProduct(@RequestBody ProductDto productDto) {
+        return new ProductDto(1L, "name", "description", 1.0);
+    }
+    @PostMapping
+    public void createProduct(@PathVariable ProductDto productDto) {
+    }
 }
